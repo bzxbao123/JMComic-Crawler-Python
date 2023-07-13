72733cf5b36ab40d3615ef4c91b583a9528c802a
bzxbao123 <45854057+bzxbao123@users.noreply.github.com>
2023-07-13 23:28:24 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -1,13 +1,14 @@
 # 下方填入你要下载的本子的id，一行一个。
 # 每行的首尾可以有空白字符
-jm_albums = JM383828
+jm_albums = ...
+JM383828
 
 
 
 
 
 
-
+...
 def main():
     from jmcomic import str_to_list, download_album
     # 下载漫画
